48315598ccf071a8a9581ff7da93c38c1435d006
Paweł Pasternak <ppaste@gmail.com>
2024-06-28 14:03:05 +0200

    income expense bug

--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -6,11 +6,11 @@ const IncomeExpense = async () => {
     return ( <div className="inc-exp-container">
         <div>
             <h4>Income</h4>
-            <p className="money plus">${addCommas(Number(income?.toFixed(2)))}</p>
+            <p className="money plus">${addCommas(Number(income?.toFixed(2) ?? 0))}</p>
         </div>
         <div>
             <h4>Expense</h4>
-            <p className="money minus">${addCommas(Number(expense?.toFixed(2)))}</p>
+            <p className="money minus">${addCommas(Number(expense?.toFixed(2) ?? 0))}</p>
         </div>
     </div> );
 }
